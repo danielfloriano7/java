cba0dc4cb5c414c4cd21d006bc950216557cf4ed
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-05 19:11:31 -0300

    add estudos

--- exercicios/HoraDoSistema/src/HoraDoSistema.java
+++ exercicios/HoraDoSistema/src/HoraDoSistema.java
@@ -4,7 +4,7 @@ public class HoraDoSistema {
     public static void main(String[] args) throws Exception {
         Date relogio = new Date();
 
-        System.out.println("A hora do sistema é");
+        System.out.print("A hora do sistema é ");
         System.out.println(relogio.toString()); 
     }
 }
