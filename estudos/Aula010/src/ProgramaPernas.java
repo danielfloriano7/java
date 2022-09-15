4747791a9d6f6c9e8ad8a110b81cdd86338ef54f
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-15 12:19:59 -0300

    estudos

---
+++ estudos/Aula010/src/ProgramaPernas.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+
+public class ProgramaPernas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
+        
+        System.out.print("Quantas pernas?");
+        int perna = tec.nextInt();
+        String tipo;
+        System.out.print("Isso é um(a) ");
+        
+        switch (perna) {
+            case 1:
+                tipo = "Saci";
+                break;
+            case 2:
+                tipo = "Bípede";
+                break;
+            case 3:
+                tipo = "Tripé";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6, 8:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";       
+        }
+            System.out.println(tipo);
+    }
+    
+}
