4415966799f6167d1ee481860fe10274ba01698a
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-19 16:33:44 -0300

    estudos

---
+++ estudos/Aula013/src/aula013/RepeticaoFor.java
@@ -0,0 +1,16 @@
+
+package aula013;
+
+public class RepeticaoFor {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (float cc = 0; cc <= 3; cc++) {
+            for (float i = 0; i <= 9; i++) {
+                System.out.printf("Cambalhota %.0f %.0f \n",cc,i);
+            }
+            
+        }
+    }
+    
+}
