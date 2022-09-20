5e8962ad67e2ffaf1e525087d646e9428ed19abc
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-20 17:00:18 -0300

    estudos

---
+++ estudos/Aula012/src/aula/Repeticao01.java
@@ -0,0 +1,16 @@
+
+package aula;
+
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+        
+        do {            
+            System.out.println(cc);
+            cc++;
+        } while (cc<4);
+    }
+    
+}
