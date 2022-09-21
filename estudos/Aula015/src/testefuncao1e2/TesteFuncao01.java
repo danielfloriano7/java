231d3469a471e247a6709bc772a16ffadd04e653
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-21 17:35:38 -0300

    estudos

---
+++ estudos/Aula015/src/testefuncao1e2/TesteFuncao01.java
@@ -0,0 +1,18 @@
+package testefuncao1e2;
+
+
+public class TesteFuncao01 {
+    
+    static void soma(int a, int b) {
+        int s = a + b;
+        System.out.println("A soma é " + s);
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Começou o programa");
+        soma(5,2);
+    }
+    
+}
---
+++ estudos/Aula015/src/testefuncao1e2/TesteFuncao02.java
@@ -0,0 +1,16 @@
+package testefuncao1e2;
+
+
+public class TesteFuncao02 {
+
+    static  int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int res =  soma(5,2);
+        System.out.println("Soma: " + res);
+    }
+    
+}
