84066d470ac31402bd853bb7e8c1bc9b87d49711
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-21 17:35:51 -0300

    exercicios

---
+++ exercicios/Funcao01/src/funcao01/Funcao01.java
@@ -0,0 +1,21 @@
+
+package funcao01;
+
+
+
+/**
+ * @author danie
+ */
+public class Funcao01 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fatorial f = new Fatorial();
+        
+        f.setValor(8);
+        
+        System.out.println(f.getFormula() + f.getFatorial());
+    }
+    
+}
