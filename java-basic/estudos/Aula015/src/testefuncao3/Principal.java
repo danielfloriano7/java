43683194aa263e471d8d7b7af98eda343b19bb3a
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-22 14:33:23 -0300

    tidying up

---
+++ java-basic/estudos/Aula015/src/testefuncao3/Principal.java
@@ -0,0 +1,16 @@
+
+package testefuncao3;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+        System.out.println("Vai comecar a contagem");
+        System.out.println(Operacoes.contador(1, 5));
+        
+        System.out.println(Math.pow(8,2));
+    }
+    
+}
