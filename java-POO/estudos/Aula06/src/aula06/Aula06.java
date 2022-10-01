802e011007a40df77fa2a0e312ca1001fce1b5e9
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-09-30 21:04:24 -0300

    estudos

---
+++ java-POO/estudos/Aula06/src/aula06/Aula06.java
@@ -0,0 +1,19 @@
+
+package aula06;
+
+public class Aula06 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ControleRemoto c = new ControleRemoto();
+        
+        c.ligar();
+        c.maisVolume();
+        c.menosVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+        
+    }
+    
+}
